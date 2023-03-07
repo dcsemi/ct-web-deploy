6cc5c82c8d3f9f43650dc7d6076322f2b5001c70
dcsemi <dcsemi@gmail.com>
2023-03-07 09:24:52 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let parallaxMenu = document.querySelector(".menu-parallax")
+function showMenu(){
+    parallaxMenu.style.display = "block"
+    return false;
+}
+function hideMenu(){
+    parallaxMenu.style.display = "none"
+}
+
+
+
+// function tick(){
+//     console.log('i am ticking')
+    
+// }
